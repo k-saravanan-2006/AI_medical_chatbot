f236882d6cb16372e3611e3ef911ec9f601d812f
k-saravanan-2006 <saravanankumar8285@gmail.com>
2026-02-08 12:04:51 +0530

    create local package using setyp.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='medicalchatbot',
+    version='0.0.1',
+    author='Saravanan Kumar',
+    author_email='saravanankumar8285@gmail.com',
+    install_requires=[],
+    packages=find_packages()
+)
